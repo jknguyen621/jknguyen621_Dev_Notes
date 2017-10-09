aa0ec4475ebe1f13a419de55416f55a313efa93f
jknguyen621 <jknguyen621@gmail.com>
2017-10-09 14:54:03 -0700

    Adding function to test yield generator, lambda and map

---
+++ Python/myYieldGeneratorLambaMap.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+def search(keyword, filename):
+    print('generator started')
+    with open(filename, 'r') as f:
+        # Looping through the file line by line
+        for line in f:
+            if keyword in line:
+                # If keyword found, return it
+                yield line
+
+
+
+def search2(keyword, filename):
+    result = []
+    [result.append(x) for x in [y for y in open(filename) if keyword in y]]
+    return result
+
+#Executing search via generator
+print "\nWill be calling search with generator and yield, uses less  memory...."
+for x in search('Python', "/Users/jknguyen/myGitRepo/Python/largeText.txt"):
+    print x
+
+#Executing it via a lists, not using generator
+print "\nWill be calling search2 with return list, uses more memory...."
+the_generator = search2('Python', "largeText.txt")
+print the_generator
+
+print "Number of iterms found: %d" % len(the_generator)
+
+
+#Review of Lamba and map(iterator):
+print "\nDefining Lambda and using map "
+greater = lambda x,y: x > y
+results = map(greater, [6,1,2,3,7,5], [2,3,4,5,5,6])
+print results
