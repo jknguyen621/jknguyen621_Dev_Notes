48d370dd6c2e8166986d1cf20f87c8973980988a
jknguyen621 <jknguyen621@gmail.com>
2017-10-09 14:56:04 -0700

    add note about not needing close with open()

--- Python/NewReadFile.py
+++ Python/NewReadFile.py
@@ -9,7 +9,7 @@ with open("../Utils/76-0.txt") as inputFile:
 			myDict.update({item:myDict[item] +1})
 		else:
 			myDict.update({item:1})
-inputFile.close()
+#inputFile.close()  #DO NOT NEED CLOSE with open...
 
 for key in sorted(myDict):
 	print"%s : %d\n" %  (key, myDict[key])
