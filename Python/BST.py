7823bd6118e35808cd327af36df87da841d23f4f
jknguyen621 <jknguyen621@gmail.com>
2017-10-12 22:59:51 -0700

    Adding Binary Search Tree to review

---
+++ Python/BST.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+#Taken from: http://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+# A utility function to search a given key in BST
+def search(root,key):
+     
+    # Base Cases: root is null or key is present at root
+    if root is None or root.val == key:
+        return root
+ 
+    # Key is greater than root's key
+    if root.val < key:
+        return search(root.right,key)
+   
+    # Key is smaller than root's key
+    return search(root.left,key)
+
+
+# Python program to demonstrate insert operation in binary search tree 
+ 
+# A utility class that represents an individual node in a BST
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+ 
+# A utility function to insert a new node with the given key
+def insert(root,node):
+    if root is None:
+        root = node
+    else:
+        if root.val < node.val:
+            if root.right is None:
+                root.right = node
+            else:
+                insert(root.right, node)
+        else:
+            if root.left is None:
+                root.left = node
+            else:
+                insert(root.left, node)
+ 
+# A utility function to do inorder tree traversal
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+ 
+ 
+# Driver program to test the above functions
+# Let us create the following BST
+#      50
+#    /    \
+#   30     70
+#   / \    / \
+#  20 40  60 80
+r = Node(50)
+insert(r,Node(30))
+insert(r,Node(20))
+insert(r,Node(40))
+insert(r,Node(70))
+insert(r,Node(60))
+insert(r,Node(80))
+ 
+# Print inoder traversal of the BST
+inorder(r)
