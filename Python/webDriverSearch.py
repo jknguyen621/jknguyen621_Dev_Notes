45b79eaa379ca7ee639362a2d589e5a0b0e0b1e8
jknguyen621 <jknguyen621@gmail.com>
2017-10-18 14:37:07 -0700

    Added WebDriver sample to load and search

---
+++ Python/webDriverSearch.py
@@ -0,0 +1,37 @@
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+import time
+
+browser = webdriver.Chrome() #webdriver.Firefox()    #webdriver.Chrome()
+browser.get("https://www.google.com/") 
+time.sleep(2)
+ 
+search = browser.find_element_by_id("lst-ib").send_keys("github/rmaschion")
+results = browser.find_element_by_id("lst-ib").send_keys(Keys.RETURN)
+
+time.sleep(3)  #Wait for page to load
+bodyText = browser.find_element_by_tag_name("body").text
+
+if "rmaschion" in bodyText:	
+	print "Found proper GitHub Repo!!!\n"
+	print bodyText
+else:
+	print "rmashion Repo not found in Search Return!\n"
+	assert( "rmaschion" in bodyText)
+
+
+
+######################################################################
+#Getting Class methods for webdriver/browser object:
+"""
+browser = webdriver.Chrome()
+>>> print dir(browser.__class__)
+['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_unwrap_value', '_web_element_cls', '_wrap_value', 'add_cookie', 'application_cache', 'back', 'close', 'create_options', 'create_web_element', 'current_url', 'current_window_handle', 'delete_all_cookies', 'delete_cookie', 'desired_capabilities', 'execute', 'execute_async_script', 'execute_script', 'file_detector', 'file_detector_context', 'find_element', 'find_element_by_class_name', 'find_element_by_css_selector', 'find_element_by_id', 'find_element_by_link_text', 'find_element_by_name', 'find_element_by_partial_link_text', 'find_element_by_tag_name', 'find_element_by_xpath', 'find_elements', 'find_elements_by_class_name', 'find_elements_by_css_selector', 'find_elements_by_id', 'find_elements_by_link_text', 'find_elements_by_name', 'find_elements_by_partial_link_text', 'find_elements_by_tag_name', 'find_elements_by_xpath', 'forward', 'get', 'get_cookie', 'get_cookies', 'get_log', 'get_network_conditions', 'get_screenshot_as_base64', 'get_screenshot_as_file', 'get_screenshot_as_png', 'get_window_position', 'get_window_rect', 'get_window_size', 'implicitly_wait', 'launch_app', 'log_types', 'maximize_window', 'mobile', 'name', 'orientation', 'page_source', 'quit', 'refresh', 'save_screenshot', 'set_network_conditions', 'set_page_load_timeout', 'set_script_timeout', 'set_window_position', 'set_window_rect', 'set_window_size', 'start_client', 'start_session', 'stop_client', 'switch_to', 'switch_to_active_element', 'switch_to_alert', 'switch_to_default_content', 'switch_to_frame', 'switch_to_window', 'title', 'window_handles']
+"""
+
+#Getting supported method for Webdrive WebElement object:
+"""
+print dir(webdriver.remote.webelement.WebElement.__class__)
+['__abstractmethods__', '__base__', '__bases__', '__basicsize__', '__call__', '__class__', '__delattr__', '__dict__', '__dictoffset__', '__doc__', '__eq__', '__flags__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__instancecheck__', '__itemsize__', '__le__', '__lt__', '__module__', '__mro__', '__name__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasscheck__', '__subclasses__', '__subclasshook__', '__weakrefoffset__', 'mro']
+"""
+
