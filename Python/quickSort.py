97f9662b541a7e08253d72109da6014837a0f195
jknguyen621 <jknguyen621@gmail.com>
2017-11-04 22:04:39 -0700

    QuickSort in Python, using Pivot point

---
+++ Python/quickSort.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+#QuickSort in Python
+
+def sort(array=[12,4,5,6,7,3,1,15]):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            if x == pivot:
+                equal.append(x)
+            if x > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to hande the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return array
+
+
+if __name__ == '__main__':
+	testArray = [7,8,9,10,45,23,11,4,2,1,21,16]
+	print("inputArray %d\n", testArray)
+        results = sort(testArray)
+        print results
