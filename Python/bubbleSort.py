235416bf8377923b4bfaf61f489caf3914cc2fd1
jknguyen621 <jknguyen621@gmail.com>
2017-11-04 22:14:39 -0700

    Bubble Sort in Python

---
+++ Python/bubbleSort.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#BubbleSort in Python
+
+def bubbleSort(inList):
+    for passnum in range(len(inList)-1,0,-1):
+        for i in range(passnum):
+            if inList[i]>inList[i+1]:
+                temp = inList[i]
+                inList[i] = inList[i+1]
+                inList[i+1] = temp
+    return inList
+
+if __name__ == '__main__':
+	myArray = [54,26,93,17,77,31,44,55,20]
+        print("Before Sort: %d\n", myArray)
+	sorted = bubbleSort(myArray)
+	print("After Sorted: %d\n", sorted)	
