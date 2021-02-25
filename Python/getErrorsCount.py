4867f9de65c7407b9c18d06847ad392b92eed55b
jknguyen621 <jknguyen621@gmail.com>
2021-02-24 23:48:50 -0800

    Updated getErrorsCount.py and cleaned up with comments

--- Python/getErrorsCount.py
+++ Python/getErrorsCount.py
@@ -1,3 +1,9 @@
+'''
+@Author: Jospeh K. Nguyen
+This program will find the error counts per line(example from textfile or logfile),
+and return the proper count for each of the error type.
+'''
+
 errors = ['cat', 'dog']
 lines = ['abc cat dog', 'bat tab dog']
 
@@ -5,16 +11,18 @@ errDict = {}
 
 
 def errsFind(errDict, errors, lines):
+    # Load up a dictionary
     for err in errors:
         errDict[err] = 0
 
+    # Get list from each items in lines
     for items in lines:
         wordList = items.split(' ')
 
-        # get error counts
+        # get 'error' word counts present in lines
         countErrorsInLogLine(errDict, wordList)
 
-
+#counting errors present in each word list from each line.
 def countErrorsInLogLine(errDict, wordList):
     for err in errDict:
         for word in wordList:
@@ -22,8 +30,6 @@ def countErrorsInLogLine(errDict, wordList):
                 errDict[err] = errDict[err] + 1
 
 
-
-
 errsFind(errDict, errors, lines)
 for err in errDict:
-    print("Error %s count is%d \n", err, errDict[err])
+    print("Error \'%s\' count is \'%d\' \n" % (err, errDict[err]))
