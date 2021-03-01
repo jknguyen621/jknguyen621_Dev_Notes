27142c572128650dcbb0ae5f7f47deb4860cd1bb
jknguyen621 <jknguyen621@gmail.com>
2021-03-01 14:07:35 -0800

    Add verbage to WriteToReadFromAppend.py

--- Python/WriteToReadFromAppend.py
+++ Python/WriteToReadFromAppend.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python
+"""
+@Author: Jospeh K. Nguyen
+This program will write "w+" to file starting at 0.  Rewind to read back and print out.
+Then reopen as append "a" to append new text at end of file.
+"""
 import os
 
 
@@ -15,14 +19,16 @@ print ("This concludes the Writing Test portion!\n")
 
 #Read
 fileOut.seek(0, os.SEEK_SET)   #Rewind file
+print ("Rewind to start of file and read back!\n")
 for lines in fileOut:
     print lines
     words = lines.split(' ')
-    print words
+    #print words
 print ("This concludes the Reading Test portion!\n")
 fileOut.close()
 
 #Append
+print ("About to append new text to existing file....\n")
 fileOut = open(DirPath+testFile, "a")
 fileOut.write("Newly Appended Text!!!\n")
 print ("This concludes the Appending Test portion!\n")
