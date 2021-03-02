672d3c95f4f341de4d5f86ea1224d245a3fede4f
jknguyen621 <jknguyen621@gmail.com>
2021-03-01 23:11:40 -0800

    Cleaned up LongestSubStringWithoutRepeatingChars.py

--- Python/LongestSubStringWithoutRepeatingChars.py
+++ Python/LongestSubStringWithoutRepeatingChars.py
@@ -1,25 +1,21 @@
-#!/usr/bin/python
+"""
+@Author: Joseph K. Nguyen
 
 #LongestSubStringWithoutRepeatingChars.py
-
-import sys, os
-
-
-"""
 Given "abcabcbb", the answer is "abc", which the length is 3.
 
 Given "bbbbb", the answer is "b", with the length of 1.
 
-Given "pwwkew", the answer is "wke", with the length of 3. 
-Note that the answer must be a substring, 
+Given "pwwkew", the answer is "wke", with the length of 3.
+Note that the answer must be a substring,
 "pwke" is a subsequence and not a substring.
-
 """
+
+import sys, os
 
 class Solution(object):
     def __init__(self, s):
         self.s = s
-
 
     def lengthOfLongestSubstring(self,s):
         """
@@ -40,14 +36,10 @@ class Solution(object):
                 sol = ch
             else:
                 pass
-
         print sol
         return len(sol)
 
-
-
 if __name__ == '__main__':
-
     myStr = "abcabcbb"   #expect: "abc"  => 3
     solve = Solution(myStr)
     sol= solve.lengthOfLongestSubstring(myStr)
